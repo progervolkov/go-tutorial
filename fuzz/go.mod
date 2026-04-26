47e418746e8149d402c9839f40bc6262d3b83b23
progervolkov <progervolkov@gmail.com>
2026-04-26 16:02:51 +0300

    add fuzz

---
+++ fuzz/go.mod
@@ -0,0 +1,3 @@
+module example/fazz
+
+go 1.26.2
---
+++ fuzz/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"unicode/utf8"
+)
+
+func Reverse(s string) (string, error) {
+	if !utf8.ValidString(s) {
+		return s, errors.New("input is not valid UTF-8")
+	}
+	r := []rune(s)
+	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
+		r[i], r[j] = r[j], r[i]
+	}
+	return string(r), nil
+}
+
+func main() {
+	input := "The quick brown fox jumped over the lazy dog"
+	rev, err := Reverse(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	doubleRev, err := Reverse(rev)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("original: %q\n", input)
+	fmt.Printf("reversed: %q\n", rev)
+	fmt.Printf("reversed again: %q\n", doubleRev)
+}
